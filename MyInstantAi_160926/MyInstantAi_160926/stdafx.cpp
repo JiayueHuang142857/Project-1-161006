f861dcce6281376d58e5a9fcc6521a5f0acbd2a1
JiayueHuang142857 <huangjy142857@163.com>
2016-09-27 14:08:49 +0800

    （2）
    信号数据以文件格式保存实现

---
+++ MyInstantAi_160926/MyInstantAi_160926/stdafx.cpp
@@ -0,0 +1,15 @@
+// stdafx.cpp : 只包括标准包含文件的源文件
+// MyInstantAi_160926.pch 将作为预编译头
+// stdafx.obj 将包含预编译类型信息
+
+#include "stdafx.h"
+
+// TODO: 在 STDAFX.H 中引用任何所需的附加头文件，
+//而不是在此文件中引用
+#include <stdlib.h>
+#include <stdio.h>
+#include "stdafx.h"
+#include "bdaqctrl.h"
+#include "compatibility.h"
+#include <fstream>
+using namespace std; 
